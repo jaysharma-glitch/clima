08bff7833d8acdda29e352d5c615dbee501d2f00
Jay Sharma <jaysharma@Jays-MacBook-Pro.local>
2021-01-19 14:42:29 +0530

    final

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -1,12 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:clima/utilities/constants.dart';
+import '../services/weather.dart';
+import 'city_screen.dart';
+
 
 class LocationScreen extends StatefulWidget {
+  LocationScreen(this.locationWeather);
+  final locationWeather;
   @override
   _LocationScreenState createState() => _LocationScreenState();
 }
 
 class _LocationScreenState extends State<LocationScreen> {
+  WeatherModel weather = WeatherModel();
+
+  int temp;
+  String weatherIcon;
+  String cityName;
+  String message;
+
+  @override
+  void initState() {
+    super.initState();
+    updateUi(widget.locationWeather);
+  }
+
+  void updateUi(dynamic weatherData){
+    setState(() {
+      if(weatherData == null){
+        temp = 0;
+        weatherIcon = "Error";
+        message = "Unable to get weather data";
+        cityName ='';
+        return;
+      }
+      double tempe= weatherData["main"]['temp'];
+      temp = tempe.toInt();
+      var condition = weatherData['weather'][0]['id'];
+      cityName = weatherData['name'];
+      weatherIcon = weather.getWeatherIcon(condition);
+      message= weather.getMessage(temp);
+    });
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,14 +66,24 @@ class _LocationScreenState extends State<LocationScreen> {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
                   FlatButton(
-                    onPressed: () {},
+                    onPressed: () async{
+                        var weatherData = await weather.getLocationWeather();
+                        updateUi(weatherData);
+                    },
                     child: Icon(
                       Icons.near_me,
                       size: 50.0,
                     ),
                   ),
                   FlatButton(
-                    onPressed: () {},
+                    onPressed: () async{
+                      var typedName = await Navigator.push(context,
+                          MaterialPageRoute(builder: (context)=>CityScreen()));
+                      if(typedName != null){
+                        var weatherData = await weather.getCityWeather(typedName);
+                        updateUi(weatherData);
+                      }
+                    },
                     child: Icon(
                       Icons.location_city,
                       size: 50.0,
@@ -49,11 +96,11 @@ class _LocationScreenState extends State<LocationScreen> {
                 child: Row(
                   children: <Widget>[
                     Text(
-                      '32°',
+                      '$temp°',
                       style: kTempTextStyle,
                     ),
                     Text(
-                      '☀️',
+                      weatherIcon,
                       style: kConditionTextStyle,
                     ),
                   ],
@@ -62,7 +109,7 @@ class _LocationScreenState extends State<LocationScreen> {
               Padding(
                 padding: EdgeInsets.only(right: 15.0),
                 child: Text(
-                  "It's 🍦 time in San Francisco!",
+                  "$message in $cityName!",
                   textAlign: TextAlign.right,
                   style: kMessageTextStyle,
                 ),
@@ -74,3 +121,4 @@ class _LocationScreenState extends State<LocationScreen> {
     );
   }
 }
+
--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,4 +1,28 @@
+import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
+
+const apiKey = '98ca6b25298265f1b63de69adb3b9279';
+const mainUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+
 class WeatherModel {
+
+  Future<dynamic> getCityWeather(String cityName) async{
+    var cityUrl = '$mainUrl?q=$cityName&appid=$apiKey&units=metric';
+    Network network = Network(cityUrl);
+
+    var weatherData = await network.getData();
+    return weatherData;
+  }
+
+  Future<dynamic> getLocationWeather() async{
+    Location location = Location();
+    await location.getCurrentLocation();
+    Network network = Network('$mainUrl?lat=${location.lat}&lon=${location.long}&appid=$apiKey&units=metric');
+      var weatherData = await network.getData();
+    return weatherData;
+  }
+
   String getWeatherIcon(int condition) {
     if (condition < 300) {
       return '🌩';
